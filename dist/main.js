91f39d3d1b0a116e69479e38e76e2d538df68c3d
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-05-10 00:09:36 +0300

    Add board

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,36 @@
 "use strict";
+const boardEl = document.querySelector('.board');
+const board = [
+    [4, 5, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 0, 7, 0, 6, 3, 0],
+    [0, 0, 0, 0, 0, 0, 0, 2, 8],
+    [0, 0, 0, 9, 5, 0, 0, 0, 0],
+    [0, 8, 6, 0, 0, 0, 2, 0, 0],
+    [0, 2, 0, 6, 0, 0, 7, 5, 0],
+    [0, 0, 0, 0, 0, 0, 4, 7, 6],
+    [0, 7, 0, 0, 4, 5, 0, 0, 0],
+    [0, 0, 8, 0, 0, 9, 0, 0, 0],
+];
+const solution = [
+    [4, 5, 3, 8, 2, 6, 1, 9, 7],
+    [8, 9, 2, 5, 7, 1, 6, 3, 4],
+    [1, 6, 7, 4, 9, 3, 5, 2, 8],
+    [7, 1, 4, 9, 5, 2, 8, 6, 3],
+    [5, 8, 6, 1, 3, 7, 2, 4, 9],
+    [3, 2, 9, 6, 8, 4, 7, 5, 1],
+    [9, 3, 5, 2, 1, 8, 4, 7, 6],
+    [6, 7, 1, 3, 4, 5, 9, 8, 2],
+    [2, 4, 8, 7, 6, 9, 3, 1, 5],
+];
+const setGame = function () {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const tyle = document.createElement('div');
+            tyle.classList.add('tile');
+            if (board[i][j] !== 0)
+                tyle.textContent = `${board[i][j]}`;
+            boardEl === null || boardEl === void 0 ? void 0 : boardEl.appendChild(tyle);
+        }
+    }
+};
+setGame();
